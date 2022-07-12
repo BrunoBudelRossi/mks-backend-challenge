07cb3390f5ee3d1716a5f85a0c3fa18a2592cfed
BrunoBudelRossi <bbrossi@inf.ufsm.br>
2022-07-11 22:21:28 -0300

    fix: import movie problem

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './User/user.module';
 import UserEntity from './db/user.entity';
-import MovieModule from './movie/movie.module';
+import MovieModule from './Movie/movie.module';
 import GenreModule from './Genre/genre.module';
 import MovieEntity from './db/movie.entity';
 import GenreEntity from './db/genre.entity';
